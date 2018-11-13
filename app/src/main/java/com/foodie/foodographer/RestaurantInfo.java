c1ed5c956e0bb6f119726f2294ca703b18b754e7
Bingxue Ouyang <bing@college10-100-64-59-36.resnet.ucsc.edu>
2018-11-12 16:51:55 -0800

    merge Michael's onDataChange

--- app/src/main/java/com/foodie/foodographer/RestaurantInfo.java
+++ app/src/main/java/com/foodie/foodographer/RestaurantInfo.java
@@ -5,14 +5,20 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
@@ -32,31 +38,38 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 import android.widget.RatingBar;
 import android.widget.ImageView;
 import android.support.v7.widget.CardView;
 
-public class RestaurantInfo extends AppCompatActivity implements View.OnClickListener{
-
+public class RestaurantInfo extends AppCompatActivity implements View.OnClickListener {
     private CardView amenties;
     private TextView restName;
     private LinearLayout restTags;
+    private DatabaseReference commentRef;
+    private DatabaseReference userRef;
+    private FirebaseAuth mAuthSetting;
+    private String currentUserID;
+    private RecyclerView myView;
+    private ArrayList<Review> reviewArrayList;
+    String Rest_ID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_info);
 
-        restTags = findViewById(R.id.rest_tags);
-        restTags.setVisibility(View.GONE);
+        mAuthSetting = FirebaseAuth.getInstance();
 
-        amenties = findViewById(R.id.openTag);
-        amenties.setOnClickListener(this);
-
-
+        //Rest_ID = getIntent().getStringExtra("myRest");
         Restaurant myRest = getIntent().getParcelableExtra("myRest");
 
         restName = findViewById(R.id.rest_name);
@@ -71,24 +84,55 @@ public class RestaurantInfo extends AppCompatActivity implements View.OnClickLis
         ImageView restIMG = findViewById(R.id.rest_IMG);
         new DownloadImageTask(restIMG).execute(myRest.getIMGURL());
 
+        Rest_ID = myRest.getId();
+
+        commentRef = FirebaseDatabase.getInstance().getReference("Restaurants").child(Rest_ID).child("comments");
+
+        myView = (RecyclerView) findViewById(R.id.recyclerview);
+        reviewArrayList = new ArrayList<>();
+
+        commentRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Log.i("snapshot", "Inside onDataChange!!!");
+                reviewArrayList.clear();
+                for(DataSnapshot reviewSnapshot: dataSnapshot.getChildren()){
+                    Log.i("database info", "Check: " + reviewSnapshot.getValue());
+                    Review thisReview = reviewSnapshot.getValue(Review.class);
+                    reviewArrayList.add(thisReview);
+                }
+                RecyclerReviewList adapter = new RecyclerReviewList(reviewArrayList);
+                myView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        restTags = findViewById(R.id.rest_tags);
+        restTags.setVisibility(View.GONE);
+
+        amenties = findViewById(R.id.openTag);
+        amenties.setOnClickListener(this);
 
 
-
-
-
-        ArrayList<Review> reviews = new ArrayList<Review>(10);
-
-        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float)3.0, "3 months ago", "Had a wonderful Sunday breakfast with good friends. My buddy had the Eggs Benedict and said it was the best he even had. I wanted a bite but it disappeared off his play too fast. Both the mushroom and western omelettes are very good. To top it off excellent delightful service. Thank you Kay!"));
-        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float)3.0, "3 months ago","content"));
-        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float)3.0, "3 months ago","content"));
-        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float)3.0, "3 months ago","content"));
-        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float)3.0, "3 months ago","content"));
+//
+//        ArrayList<Review> reviews = new ArrayList<Review>();
+//
+//        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float) 3.0, "3 months ago", "Had a wonderful Sunday breakfast with good friends. My buddy had the Eggs Benedict and said it was the best he even had. I wanted a bite but it disappeared off his play too fast. Both the mushroom and western omelettes are very good. To top it off excellent delightful service. Thank you Kay!"));
+//        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float) 3.0, "3 months ago", "content"));
+//        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float) 3.0, "3 months ago", "content"));
+//        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float) 3.0, "3 months ago", "content"));
+//        reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float) 3.0, "3 months ago", "content"));
 
         // making articles recycle
-        RecyclerReviewList adapter = new RecyclerReviewList(reviews);
-        RecyclerView myView =  (RecyclerView)findViewById(R.id.recyclerview);
+//        RecyclerReviewList adapter = new RecyclerReviewList(reviews);
         myView.setHasFixedSize(true);
-        myView.setAdapter(adapter);
+//        myView.setAdapter(adapter);
+//        RecyclerView myView = (RecyclerView) findViewById(R.id.recyclerview);
+
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         myView.setLayoutManager(llm);
@@ -99,12 +143,50 @@ public class RestaurantInfo extends AppCompatActivity implements View.OnClickLis
         if (view == amenties) {
             if (restTags.getVisibility() != View.VISIBLE) {
                 restTags.setVisibility(View.VISIBLE);
-            }
-            else{
+            } else {
                 restTags.setVisibility(View.GONE);
             }
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    /**
+     * Go to Post Review Page and wait for user's input
+     *
+     * @param view
+     */
+    public void postReview(View view) {
+        Intent intent = new Intent(RestaurantInfo.this, PostReviewActivity.class);
+        startActivityForResult(intent, 21);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 21 && resultCode == RESULT_OK) {
+            String userReview = data.getStringExtra("userReview");
+            saveReviewToDB(userReview);
+        }
+    }
+
+    private void saveReviewToDB(String userReview){
+        currentUserID=mAuthSetting.getCurrentUser().getUid();
+        userRef = FirebaseDatabase.getInstance().getReference("users").child(currentUserID).child("comments").child(Rest_ID);
+        String uid = currentUserID;
+        String email = mAuthSetting.getCurrentUser().getEmail();
+        String emailUserName = email.substring(0,email.indexOf('@'));
+        Review testReview = new Review(emailUserName,"https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", (float) 3.0, "3 months ago", userReview);
+        HashMap<String, Object> restaurantParams = new HashMap<>();
+        restaurantParams.put(uid, testReview);
+        commentRef.updateChildren(restaurantParams);
+        HashMap<String, Object> userParams = new HashMap<>();
+        userParams.put("content", userReview);
+        userRef.updateChildren(userParams);
+    }
 
 }
---
+++ app/src/main/java/com/foodie/foodographer/User.java
@@ -0,0 +1,132 @@
+package com.foodie.foodographer;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.yelp.fusion.client.models.SearchResponse;
+
+import retrofit2.Callback;
+
+
+public class User implements Parcelable {
+    private DatabaseReference userRefer;
+    public String IMGURL, email, expert1, expert2, expert3, interest1, interest2, interest3, interest4;
+
+    public User(String email, String IMGURL, String expert1, String expert2, String expert3, String interest1, String interest2, String interest3, String interest4){
+        //this.uid = uid;
+        this.email = email;
+        this.IMGURL = IMGURL;
+        this.expert1 = expert1;
+        this.expert2 = expert2;
+        this.expert3 = expert3;
+        this.interest1 = interest1;
+        this.interest2 = interest2;
+        this.interest3 = interest3;
+        this.interest4 = interest4;
+        /*
+        readData(new MyCallback() {
+            @Override
+            public void onCallback(String email, String IMGURL, String expert1, String expert2, String expert3, String interest1, String interest2, String interest3, String interest4) {
+                setEmail(email);
+                setIMGURL(IMGURL);
+            }
+        });
+        */
+        // get user object from firebase by uid
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+        Log.i("ssss", email);
+    }
+    public String getEmail(){
+        //Log.i("ssss",this.email);
+        return this.email;
+    }
+
+    public void setIMGURL(String IMGURL){
+        this.IMGURL = IMGURL;
+    }
+    public String getIMGURL(){
+        return this.IMGURL;
+    }
+
+    public String getExpert1(){
+        return this.expert1;
+    }
+
+    public String getExpert2(){
+        return this.expert2;
+    }
+
+    public String getExpert3(){
+        return this.expert3;
+    }
+
+    public String getInterest1() { return this.interest1; }
+
+    public String getInterest2() { return this.interest2; }
+
+    public String getInterest3() { return this.interest3; }
+
+    public String getInterest4() { return this.interest4; }
+
+    public User(Parcel in){
+        //uid, IMGURL, email, expert1, expert2, expert3, interest1, interest2, interest3, interest4;
+        this.email = in.readString();
+        this.IMGURL = in.readString();
+        this.expert1 = in.readString();
+        this.expert2 = in.readString();
+        this.expert3 = in.readString();
+        this.interest1 = in.readString();
+        this.interest2 = in.readString();
+        this.interest3 = in.readString();
+        this.interest4 = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        //  TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // TODO Auto-generated method stub
+        dest.writeString(this.email);
+        dest.writeString(this.IMGURL);
+        dest.writeString(this.expert1);
+        dest.writeString(this.expert2);
+        dest.writeString(this.expert3);
+        dest.writeString(this.interest1);
+        dest.writeString(this.interest2);
+        dest.writeString(this.interest3);
+        dest.writeString(this.interest4);
+    }
+
+    public static final Parcelable.Creator<User> CREATOR= new Parcelable.Creator<User>() {
+
+        @Override
+        public User createFromParcel(Parcel source) {
+            // TODO Auto-generated method stub
+            return new User(source);  //using parcelable constructor
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            // TODO Auto-generated method stub
+            return new User[size];
+        }
+    };
+
+
+}
