a7af7aee4f088595ccc79e1c783448edc3b9882f
Jinghao Hao <jeremyhao1125@outlook.com>
2018-11-13 15:20:31 -0800

    working on buttons on restaurant info page

--- app/src/main/java/com/foodie/foodographer/RestaurantInfo.java
+++ app/src/main/java/com/foodie/foodographer/RestaurantInfo.java
@@ -1,8 +1,10 @@
 package com.foodie.foodographer;
 
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -44,6 +46,12 @@ public class RestaurantInfo extends AppCompatActivity implements View.OnClickLis
     private CardView amenties;
     private TextView restName;
     private LinearLayout restTags;
+    private CardView openTime;
+    private TextView time_info;
+    private ImageButton likeButton;
+    private boolean isLiked = false;
+    private LinearLayout getDirection;
+    private LinearLayout Call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +63,11 @@ public class RestaurantInfo extends AppCompatActivity implements View.OnClickLis
         amenties = findViewById(R.id.openTag);
         amenties.setOnClickListener(this);
 
+        //bookmark button
+        likeButton = findViewById(R.id.likeButton);
+        likeButton.setOnClickListener(this);
 
+        //restaurant name, ratings, locations, # of reviews
         Restaurant myRest = getIntent().getParcelableExtra("myRest");
 
         restName = findViewById(R.id.rest_name);
@@ -70,11 +82,24 @@ public class RestaurantInfo extends AppCompatActivity implements View.OnClickLis
         ImageView restIMG = findViewById(R.id.rest_IMG);
         new DownloadImageTask(restIMG).execute(myRest.getIMGURL());
 
+        TextView rest_review_numbers = findViewById(R.id.rest_review_numbers);
+        rest_review_numbers.setText(myRest.getReviewCount());
 
+        //expandable time info
+        openTime = findViewById(R.id.openTime);
+        openTime.setOnClickListener(this);
 
+        time_info = (TextView) findViewById(R.id.time_info);
+        time_info.setVisibility(View.GONE);
 
+        //get direction to restaurant and call restaurant
+        getDirection = findViewById(R.id.get_direction);
+        Call = findViewById(R.id.call);
 
+        getDirection.setOnClickListener(this);
+        Call.setOnClickListener(this);
 
+        //10 Review lists
         ArrayList<Review> reviews = new ArrayList<Review>(10);
 
         reviews.add(new Review("https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png", "Haofan", (float)3.0, "3 months ago", "Had a wonderful Sunday breakfast with good friends. My buddy had the Eggs Benedict and said it was the best he even had. I wanted a bite but it disappeared off his play too fast. Both the mushroom and western omelettes are very good. To top it off excellent delightful service. Thank you Kay!"));
@@ -94,6 +119,7 @@ public class RestaurantInfo extends AppCompatActivity implements View.OnClickLis
         myView.setLayoutManager(llm);
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     public void onClick(View view) {
         if (view == amenties) {
@@ -104,6 +130,38 @@ public class RestaurantInfo extends AppCompatActivity implements View.OnClickLis
                 restTags.setVisibility(View.GONE);
             }
         }
+
+        if(view==openTime){
+            if (time_info.getVisibility() != View.VISIBLE) {
+                time_info.setVisibility(View.VISIBLE);
+            }
+            else{
+                time_info.setVisibility(View.GONE);
+            }
+        }
+
+        if(view==likeButton){
+            isLiked=!isLiked;
+
+            if(isLiked==true){
+                likeButton.setBackgroundColor(R.color.colorPrimaryDark);
+            } else{
+                likeButton.setBackgroundColor(R.color.colorBookmark_no);
+            }
+        }
+
+        if(view==getDirection){
+
+        }
+
+        if(view==Call){
+
+
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:123456789"));
+            startActivity(callIntent);
+        }
+
     }
 
 
