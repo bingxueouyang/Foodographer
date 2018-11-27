1894a45a1555e90dd5fd6524030dea8f738cef8e
sishenyelang <jinxiao5201314@gmail.com>
2018-11-27 14:42:14 -0800

    finish comment usercomment

--- app/src/main/java/com/foodie/foodographer/UserCommentRestaurant.java
+++ app/src/main/java/com/foodie/foodographer/UserCommentRestaurant.java
@@ -1,5 +1,5 @@
 package com.foodie.foodographer;
-
+//import eveything need for this java class
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
