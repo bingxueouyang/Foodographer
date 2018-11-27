ed4e7f0bc6962051e3e740ad15051007e88f6893
Bingxue Ouyang <43938500+BingxueOuyang@users.noreply.github.com>
2018-11-27 11:18:47 -0800

    comment RecyclerArticle

--- app/src/main/java/com/foodie/foodographer/RecyclerArticleList.java
+++ app/src/main/java/com/foodie/foodographer/RecyclerArticleList.java
@@ -1,5 +1,7 @@
-//https://stackoverflow.com/questions/30751870/can-one-use-cardview-for-listview-item-and-how
-
+/* 
+ * An adapter object for Article Page 
+ * https://stackoverflow.com/questions/30751870/can-one-use-cardview-for-listview-item-and-how
+ */
 package com.foodie.foodographer;
 
 import android.support.v7.widget.RecyclerView;
@@ -19,26 +21,30 @@ public class RecyclerArticleList extends RecyclerView.Adapter<RecyclerArticleLis
 
     public RecyclerArticleList (ArrayList<Article> article_list){
         this.article_list = article_list;
-
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflate the list view
         context = parent.getContext();
-        View listItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.article_card, parent, false);
+        View listItem = LayoutInflater.from(parent.getContext())
+            .inflate(R.layout.article_card, parent, false);
         return new MyViewHolder(listItem);
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        // set view content with article object value
         Article article = article_list.get(position);
         final String url = article.getArticleURL();
         holder.artTitle.setText(article.getTitle());
+        // download image from url
         new DownloadImageTask(holder.artIMG).execute(article.getIMGURL());
+        // set onClick on each item
         holder.itemView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                // jump to Article page with web view
                 Intent intent = new Intent(context, ArticlePage.class);
                 intent.putExtra("webURL", url);
                 context.startActivity(intent);
@@ -50,12 +56,14 @@ public class RecyclerArticleList extends RecyclerView.Adapter<RecyclerArticleLis
 
     @Override
     public int getItemCount() {
+        // article count
         return article_list.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView artTitle;
         private ImageView artIMG;
+        // find views and assign to holder
         public MyViewHolder(View itemView) {
             super(itemView);
             artTitle = (TextView) itemView.findViewById(R.id.title);
