deb85d5e23f4c90ae7e3cf2f58850fa05ff16db0
sishenyelang <jinxiao5201314@gmail.com>
2018-11-27 14:39:32 -0800

    finish commit resetpassword

--- app/src/main/java/com/foodie/foodographer/ResetPassword.java
+++ app/src/main/java/com/foodie/foodographer/ResetPassword.java
@@ -1,5 +1,5 @@
 package com.foodie.foodographer;
-
+// import eveything need for this java
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -13,6 +13,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 public class ResetPassword extends AppCompatActivity {
+    //initalize the button, edittext and firebase authentication
     private Button goto_email;
     private EditText user_reset_enter_email;
     private FirebaseAuth checkAuth;
@@ -20,8 +21,11 @@ public class ResetPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+        //connect to firebase authentication
         checkAuth=FirebaseAuth.getInstance();
+        // grab the button that reset email
         goto_email = (Button) findViewById(R.id.send_email);
+        // grabbing the email that user try to reset
         user_reset_enter_email=(EditText)findViewById(R.id.email_box);
 
         goto_email.setOnClickListener(new View.OnClickListener() {
@@ -31,13 +35,16 @@ public class ResetPassword extends AppCompatActivity {
                 if(TextUtils.isEmpty(email_input)){
                     Toast.makeText(ResetPassword.this, "Enter the email please",Toast.LENGTH_SHORT).show();
                 }else{
+                    //go into firebase authentication to send reset password
                     checkAuth.sendPasswordResetEmail(email_input).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if(task.isSuccessful()){
+                                //check if it is successful
                                 Toast.makeText(ResetPassword.this, "Success, Check your email",Toast.LENGTH_SHORT).show();
                                 startActivity(new Intent(ResetPassword.this,LogIn.class));
                             }else{
+                                //check if there is an error
                                 String bug_error=task.getException().getMessage();
                                 Toast.makeText(ResetPassword.this, "Error Something went wrong!"+bug_error,Toast.LENGTH_SHORT).show();
                             }
