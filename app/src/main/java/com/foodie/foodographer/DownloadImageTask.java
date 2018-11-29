d929b1b38e1c9dc3728c52d82d4105e4728b2a68
Michael Zhang <michaelzhang021@hotmail.com>
2018-11-29 14:09:43 -0800

    Comment downloadTask

--- app/src/main/java/com/foodie/foodographer/DownloadImageTask.java
+++ app/src/main/java/com/foodie/foodographer/DownloadImageTask.java
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/questions/2471935/how-to-load-an-imageview-by-url-in-android
 package com.foodie.foodographer;
-
+//import necessary libraries
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -8,15 +8,20 @@ import android.util.Log;
 import android.widget.ImageView;
 
 import java.io.InputStream;
-
+/**
+ * Download an external image by its URL and then fill it into an ImageView.
+ * Usage: new DownloadImageTask('ImageView's viewID').execute('image URL')
+ */
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
     ImageView bmImage;
-
+    
+    //fetch the targeted ImageView from the user as a constructor parameter
     public DownloadImageTask(ImageView bmImage) {
 
         this.bmImage = bmImage;
     }
-
+    
+    //download the image(Async) from its URL
     protected Bitmap doInBackground(String... urls) {
         String urldisplay = urls[0];
         Bitmap mIcon11 = null;
@@ -29,7 +34,8 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         }
         return mIcon11;
     }
-
+    
+    //if the download task finished successfully, set the image to the imageView
     protected void onPostExecute(Bitmap result) {
 
         bmImage.setImageBitmap(result);
